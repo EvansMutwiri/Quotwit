8f01d954f3bcaf99f190398986bad2f7b729adb6
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-09-04 14:56:14 +0300

    Member variable initialization

--- app/src/main/java/com/evans/quotwit/SignUpActivity.java
+++ app/src/main/java/com/evans/quotwit/SignUpActivity.java
@@ -3,12 +3,33 @@ package com.evans.quotwit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText mUsername;
+    EditText mEmail;
+    EditText mPassword;
+    EditText mRepeatPassword;
+
+    final int MIN_PASSWORD_LENGTH = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        viewInitializations();
+    }
+
+    void viewInitializations(){
+        mUsername = findViewById(R.id.et_user_name);
+        mEmail = findViewById(R.id.et_email);
+        mPassword = findViewById(R.id.et_password);
+        mRepeatPassword = findViewById(R.id.et_repeat_password);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
