669e07adcc672461a0341c1f8583ee14d81790ac
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-09-13 01:10:55 +0300

    fetch data err handling

--- app/src/main/java/com/evans/quotwit/Requests.java
+++ app/src/main/java/com/evans/quotwit/Requests.java
@@ -1,13 +1,19 @@
 package com.evans.quotwit;
 
+import static com.evans.quotwit.Constants.API_KEY;
 import static com.evans.quotwit.Constants.BASE_URL;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 import models.NewsApiResponse;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public class Requests {
@@ -19,9 +25,46 @@ public class Requests {
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
+    public void getNewsHeadlines(OnFetchData listener, String category, String query)
+    {
+        CallApi callApi = retrofit.create(CallApi.class);
+        Call<NewsApiResponse> call = callApi.callHeadlines("us", category, query, API_KEY);
+
+        try{
+            call.enqueue(new Callback<NewsApiResponse>() {
+                @Override
+                public void onResponse(Call<NewsApiResponse> call, Response<NewsApiResponse> response) {
+                    if(!response.isSuccessful()){
+                        Toast.makeText(context, "Error!!", Toast.LENGTH_LONG).show();
+                    }
+
+                    listener.onFetchData(response.body().getArticles(), response.message());
+                }
+
+                @Override
+                public void onFailure(Call<NewsApiResponse> call, Throwable t) {
+                    listener.onError("Request failed");
+                }
+            });
+        }
+
+        // error
+        catch (Exception e){
+            Log.e("Error message!", "onFailure ", e);
+        }
+    }
+
     public Requests(Context context) {
         this.context = context;
     }
 
-
+    //interface to manage api call
+    public interface CallApi {
+        @GET("top-headlines")
+        Call<NewsApiResponse> callHeadlines(
+                @Query("country") String country,
+                @Query("category") String category,
+                @Query("q") String query,
+                @Query("apiKey") String API_KEY);
+    }
 }
