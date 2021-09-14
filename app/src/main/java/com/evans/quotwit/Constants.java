c826fd370ea2a39a241c98417c11b07ba166ec33
Evans Mutwiri <82495260+EvansMutwiri@users.noreply.github.com>
2021-09-14 07:18:37 +0300

    Revert "Ft pageadapter"

--- app/src/main/java/com/evans/quotwit/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.evans.quotwit;
-
-public class Constants {
-    public static final String BASE_URL = "https://newsapi.org/v2/";
-            public static final String API_KEY = BuildConfig.API_KEY;
-}
