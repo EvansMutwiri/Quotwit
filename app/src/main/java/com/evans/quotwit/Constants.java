9832233d8842a8050eda612ef5d0dce741485c81
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-09-14 11:50:04 +0300

    add missing dependecies

--- app/src/main/java/com/evans/quotwit/Constants.java
+++ app/src/main/java/com/evans/quotwit/Constants.java
@@ -2,5 +2,5 @@
 
     public class Constants {
         public static final String BASE_URL = "https://newsapi.org/v2/";
-                public static final String API_KEY = BuildConfig.API_KEY;
+                public static final String API_KEY = "76544e7ed0d74ff3b21dccd98bf04849";
     }
