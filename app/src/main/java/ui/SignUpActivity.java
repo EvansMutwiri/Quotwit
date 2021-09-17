f1a84f906480fc01de17cb12dc84360c77d2da1b
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-09-17 22:07:36 +0300

    Registering Accounts in Firebase

--- app/src/main/java/ui/SignUpActivity.java
+++ app/src/main/java/ui/SignUpActivity.java
@@ -1,26 +1,38 @@
 package ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.evans.quotwit.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
 
+    public static final String TAG = SignUpActivity.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
+    //firebase auth listener
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @BindView(R.id.et_user_name) EditText mUsername;
     @BindView(R.id.et_email) EditText mEmail;
     @BindView(R.id.et_password) EditText mPassword;
     @BindView(R.id.et_repeat_password)EditText mRepeatPassword;
     @BindView(R.id.loginTextView) TextView mLoginBtn;
+    @BindView(R.id.bt_register) TextView createuser;
 
     final int MIN_PASSWORD_LENGTH = 6;
 
@@ -32,7 +44,11 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
         mLoginBtn.setOnClickListener(this);
+
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
         viewInitializations();
+        createAuthStateListener();
     }
 
     void viewInitializations(){
@@ -74,24 +90,50 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         if (validateInput()) {
 
             //send data to server
-
-            String username = mUsername.getText().toString();
-            String email = mEmail.getText().toString();
-            String password = mPassword.getText().toString();
-            String repeatPassword = mRepeatPassword.getText().toString();
-
-            Toast.makeText(SignUpActivity.this, "Account Created", Toast.LENGTH_SHORT).show();
+            createNewUser();
 
             //call API BUT meanwhile ill add intent to login
-            Intent login = new Intent(SignUpActivity.this, LoginActivity.class);
-            login.putExtra("username", username);
-            login.putExtra("password", password);
-            startActivity(login);
+//            Intent login = new Intent(SignUpActivity.this, LoginActivity.class);
+//            login.putExtra("username", username);
+//            login.putExtra("password", password);
+//            startActivity(login);
 
         }
 
     }
+    // create new user method on click
+    private void createNewUser() {
+        final String username = mUsername.getText().toString().trim();
+        final String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String repeatPassword = mRepeatPassword.getText().toString().trim();
 
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "Auth successful");
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "Auth failed", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Intent intent = new Intent(SignUpActivity.this, Topics.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    //LOGIN BTN INTENT
     @Override
     public void onClick(View view) {
         if (view == mLoginBtn){
@@ -99,6 +141,21 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             toLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(toLogin);
             finish();
+        }
+    }
+
+    // REMOVE AUTH LISTENER
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 }
