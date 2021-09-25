faca8d6ce258411b738fd067b1292958aee9c47e
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-09-25 04:53:50 +0300

    set username

--- app/src/main/java/ui/SignUpActivity.java
+++ app/src/main/java/ui/SignUpActivity.java
@@ -3,6 +3,7 @@ package ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -17,6 +18,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,6 +29,7 @@ import butterknife.ButterKnife;
 public class SignUpActivity extends AppCompatActivity{
 
     public static final String TAG = SignUpActivity.class.getSimpleName();
+    private String username;
 
     private FirebaseAuth mAuth;
 
@@ -36,8 +42,11 @@ public class SignUpActivity extends AppCompatActivity{
     @BindView(R.id.et_repeat_password)EditText mConfirmPasswordEditText;
     @BindView(R.id.loginTextView) TextView mLoginBtn;
     @BindView(R.id.bt_register) Button btnRegister;
-    @BindView(R.id.firebaseProgressBar) ProgressBar mSignInProgressBar;
-    @BindView(R.id.loadingTextView) TextView mLoadingSignUp;
+//    @BindView(R.id.firebaseProgressBar) ProgressBar mSignInProgressBar;
+//    @BindView(R.id.loadingTextView) TextView mLoadingSignUp;
+
+    @BindView(R.id.firebaseProgressBar) ProgressBar firebaseProgressBar;
+    @BindView(R.id.loadingTextView) TextView loadingTextView;
 
 
     @Override
@@ -57,17 +66,30 @@ public class SignUpActivity extends AppCompatActivity{
         });
     }
 
+    private void showProgressBar() {
+        firebaseProgressBar.setVisibility(View.VISIBLE);
+        loadingTextView.setVisibility(View.VISIBLE);
+        loadingTextView.setText("Sign Up process in Progress");
+    }
+
+    private void hideProgressBar() {
+        firebaseProgressBar.setVisibility(View.GONE);
+        loadingTextView.setVisibility(View.GONE);
+    }
+
     void viewInitializations(){
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void createNewUser() {
-        String username = mNameEditText.getText().toString().trim();
+        username = mNameEditText.getText().toString().trim();
         String email = mEmailEditText.getText().toString().trim();
         String password = mPasswordEditText.getText().toString();
         String repeatpass = mConfirmPasswordEditText.getText().toString();
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+//        String validUsername = username;
 
         if(TextUtils.isEmpty(username)){
             mNameEditText.setError("Name cannot be blank");
@@ -92,17 +114,40 @@ public class SignUpActivity extends AppCompatActivity{
             mConfirmPasswordEditText.setError("Password must match");
             mConfirmPasswordEditText.requestFocus();
         } else {
+            showProgressBar();
             mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
+                    hideProgressBar();
+
                     if(task.isSuccessful()){
+                        createFirebaseUserProfile(Objects.requireNonNull(task.getResult().getUser()));
                         Toast.makeText(SignUpActivity.this, "Account creation successfull", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                        startActivity(new Intent(SignUpActivity.this, Topics.class));
                     } else {
                         Toast.makeText(SignUpActivity.this, "Failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                     }
                 }
             });
         }
+    }
+    private void createFirebaseUserProfile(final FirebaseUser user) {
+
+        UserProfileChangeRequest addProfileName = new UserProfileChangeRequest.Builder()
+                .setDisplayName(username)
+                .build();
+
+        user.updateProfile(addProfileName)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+//                            Log.d(TAG, Objects.requireNonNull(user.getDisplayName()));
+                            Toast.makeText(SignUpActivity.this, "The display name has ben set", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
     }
 }
